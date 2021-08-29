b13dd55be248d2abd824fe04fdcb5faff130c2bc
LatifHa <88936617+LatifHa@users.noreply.github.com>
2021-08-28 23:11:14 -0700

    Add files via upload

---
+++ Task2_7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task2_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 123456;
+
+            int yeni = 0;
+
+            while (x > 0)
+            {
+                //int qaliq = x % 10;
+                yeni = yeni+1;
+                x = x / 10;
+                Console.WriteLine(yeni);
+            }
+
+            
+        }
+    }
+}
